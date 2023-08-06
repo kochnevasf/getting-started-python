d992c2c5d896d130d0b2425f3ebae529d34c6a14
kochnevasf <kochnevasf@mail.ru>
2023-08-06 08:59:11 +0400

    Исправила проверки

--- test_google_should_find_selene.py
+++ test_google_should_find_selene.py
@@ -7,23 +7,20 @@ import pytest
 def browser_size():
     browser.config.window_height = 1280
     browser.config.window_width = 1024
-    browser.open('https://google.com')
 
     yield
 
     browser.close()
 
+
 def test_find(browser_size):
+    browser.open('https://google.com')
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
     browser.element('[id="search"]').should(have.text('c'))
 
+
 def test_not_find1(browser_size):
+    browser.open('https://google.com')
     browser.element('[name="q"]').should(be.blank).type('н52f2fwefwefwккт0018ьллвдеткирврыы').press_enter()
-    element = browser.element('[id="search"]')
-    element.should(have.text(" "))
+    browser.element('[id="search"]').should(have.text('Похоже, по вашему запросу нет полезных результатов'))
 
-
-
-# def test_find2(browser_size):
-#      browser.element('[name="q"]').should(be.blank).type('н52ккт0018ьллвдеткирврыы').press_enter()
-#      browser.element('[id="search"]').should(have.text('Похоже, по вашему запросу нет полезных результат'))
